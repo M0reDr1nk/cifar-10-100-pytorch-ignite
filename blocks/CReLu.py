9737e132c455561922f9ef47e9b6bcce6d16606e
M0reDr1nk <1162987472@qq.com>
2019-03-15 10:15:53 +0800

    Add files via upload

---
+++ blocks/CReLu.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class CReLU(nn.Module):
+    def __init__(self):
+        super(CReLU, self).__init__()
+
+    def forward(self, x):
+        return torch.cat((F.relu(x), F.relu(-x)), 1)
